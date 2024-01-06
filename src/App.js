6a8a6e957e1b09d9d6191de5816f31ef94794522
Yash Teredesai <107024856+yasht04@users.noreply.github.com>
2024-01-06 13:37:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './component/Home';
 import Login from './component/Login';
 import Nav from './component/Nav';
+import Nav from './component/Nav';
+import Nav from './component/Nav';
 import Feature from './component/Feature';
 
 function App() {
